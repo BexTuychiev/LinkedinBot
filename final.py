ee5358e64c0a2eee4b4dbd1ebcf4bf8c20773abb
Bex Tuychiev <bex@ibexprogramming.com>
2020-06-21 15:47:31 +0500

    feat: Define a class method to get the full data of a profile

--- final.py
+++ final.py
@@ -27,3 +27,18 @@ class LinkedinBot:
 
         # Get full data of a profile
         self.results = self._get_full_data(self.mode, self.value)
+
+    def _get_full_data(self, mode, value):
+        """
+        A function method that is called in self.__init__ to get full information about the profile in a dictionary
+        :param mode: Defines the type of info that is going to be scraped, either company or individual
+        :param value: Username if individual profile is desired, or company name for otherwise
+        :return: a dictionary object containing the results of the scraper
+        """
+        if mode == 'company':
+            with CompanyScraper(self.cookie) as scraper:
+                data = scraper.scrape(company=value)
+        if mode == 'individual' or mode == 'in':
+            with ProfileScraper(self.cookie) as scraper:
+                data = scraper.scrape(user=value)
+        return data.to_dict()
